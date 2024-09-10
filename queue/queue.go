024f163476f82fd98d49cac8228b6a67c174d3c3
vedranvuk <vedranvuk82@gmail.com>
2024-09-10 17:15:59 +0200

    queue split into mutexted and non-mutexed

--- queue/queue.go
+++ queue/queue.go
@@ -2,25 +2,45 @@ package queue
 
 import "sync"
 
-type Queue[K any] struct {
-	m     sync.Mutex
-	items []K
+// Queue is a generic queue of any.
+// Items are Pushed to end of list and popped from the front.
+type Queue[V any] struct {
+	items []V
 }
 
-func (self *Queue[K]) Push(v K) {
-	self.m.Lock()
-	self.items = append(self.items, v)
-	self.m.Unlock()
-}
+// New returns a new queue of V.
+func New[V any]() *Queue[V] { return &Queue[V]{} }
 
-func (self *Queue[K]) Pop() (v K, b bool) {
-	self.m.Lock()
-	if len(self.items) == 0 {
-		self.m.Unlock()
-		return v, false
+// Push pushes v to end of queue.
+func (self *Queue[V]) Push(v V) { self.items = append(self.items, v) }
+
+// Pop returns item from the start of the queue and truth if one was found.
+// Returned value should be ignored if truth if false, it is the zero value of
+// Queue generic type.
+func (self *Queue[V]) Pop() (v V, b bool) {
+	if b = len(self.items) > 0; !b {
+		return
 	}
 	v = self.items[0]
 	self.items = self.items[1:]
-	self.m.Unlock()
-	return v, true
+	return
+}
+
+// SyncQueue is concurrency safe Queue.
+type SyncQueue[V any] struct {
+	mu sync.Mutex
+	q  *Queue[V]
+}
+
+func (self *SyncQueue[V]) Push(v V) {
+	self.mu.Lock()
+	self.q.Push(v)
+	self.mu.Unlock()
+}
+
+func (self *SyncQueue[V]) Pop() (v V, b bool) {
+	self.mu.Lock()
+	v, b = self.q.Pop()
+	self.mu.Unlock()
+	return
 }
