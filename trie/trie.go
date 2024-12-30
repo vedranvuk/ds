107b0bb9a71793a5f3b91beccb8d40954abcc501
vedranvuk <vedranvuk82@gmail.com>
2024-12-30 13:12:48 +0100

    added SyncTrie

--- trie/trie.go
+++ trie/trie.go
@@ -6,9 +6,10 @@ import (
 	"io"
 	"slices"
 	"strings"
+	"sync"
 )
 
-// Trie implements a generic prefix tree that stores any type by a string key.
+// Trie implements a prefix tree of generic values keyed by a string key.
 //
 // It has fast lookups and can retrieve keys which are a prefix of some key.
 //
@@ -21,11 +22,15 @@ import (
 // Gets are fast, require no allocations.
 type Trie[V any] struct {
 	root *Node[V]
+	zero V
 }
 
 // New returns a new [Trie].
 func New[V any]() *Trie[V] {
-	return &Trie[V]{root: new(Node[V])}
+	return &Trie[V]{
+		root: new(Node[V]),
+		zero: *new(V),
+	}
 }
 
 // Put inserts value under key.
@@ -35,105 +40,105 @@ func New[V any]() *Trie[V] {
 //
 // Key must not be empty. If it is no value is inserted and a zero value of V
 // and false is returned.
-func (self Trie[V]) Put(key string, value V) (old V, replaced bool) {
+func (self *Trie[V]) Put(key string, value V) (old V, replaced bool) {
 
 	if key == "" {
-		return *new(V), false
+		return self.zero, false
 	}
 
-	var qry = []rune(key)
-	var idx, found = self.root.Branches.find(qry[0])
+	var keyRunes = []rune(key)
+	var idx, found = self.root.Branches.find(keyRunes[0])
 
 	// fast path, no branch for key starting rune.
 	if !found {
-		var node = &Node[V]{
-			Prefix:   qry,
+		var newNode = &Node[V]{
+			Prefix:   keyRunes,
 			Value:    value,
 			HasValue: true,
 		}
-		self.root.Branches = slices.Insert(self.root.Branches, idx, node)
-		return *new(V), false
+		self.root.Branches = slices.Insert(self.root.Branches, idx, newNode)
+		return self.zero, false
 	}
 
-	var node = self.root.Branches[idx]
-	var npfx = []rune(node.Prefix)
+	var currentNode = self.root.Branches[idx]
+	var nodeRunes = []rune(currentNode.Prefix)
 
 restart:
 	var i = 0
 	for {
 		// end of query reached
-		if i == len(qry) {
+		if i == len(keyRunes) {
 
 			// Node prefix fully matched.
-			if i == len(npfx) {
-				old, replaced = node.Value, node.HasValue
-				node.Value, node.HasValue = value, true
+			if i == len(nodeRunes) {
+				old, replaced = currentNode.Value, currentNode.HasValue
+				currentNode.Value, currentNode.HasValue = value, true
 				return
 			}
 
 			// node prefix partially matched, split node.
 			var newNode = &Node[V]{
-				Prefix:   npfx[i:],
-				Value:    node.Value,
-				HasValue: node.HasValue,
-				Branches: node.Branches,
+				Prefix:   nodeRunes[i:],
+				Value:    currentNode.Value,
+				HasValue: currentNode.HasValue,
+				Branches: currentNode.Branches,
 			}
-			node.Prefix = qry
-			node.Value = value
-			node.HasValue = true
-			node.Branches = Branches[V]{newNode}
+			currentNode.Prefix = keyRunes
+			currentNode.Value = value
+			currentNode.HasValue = true
+			currentNode.Branches = Branches[V]{newNode}
 
-			return *new(V), false
+			return self.zero, false
 		}
 
 		// end of node prefix reached
-		if i == len(npfx) {
-			idx, found = node.Branches.find(qry[i])
+		if i == len(nodeRunes) {
+			idx, found = currentNode.Branches.find(keyRunes[i])
 
 			// No branches found, insert new.
 			if !found {
 				var newNode = &Node[V]{
-					Prefix:   qry[i:],
+					Prefix:   keyRunes[i:],
 					Value:    value,
 					HasValue: true,
 				}
-				node.Branches = slices.Insert(node.Branches, idx, newNode)
-				return *new(V), false
+				currentNode.Branches = slices.Insert(currentNode.Branches, idx, newNode)
+				return self.zero, false
 			}
 
 			// Set current node to node under matched branch and restart.
-			node = node.Branches[idx]
-			qry = qry[i:]
-			npfx = []rune(node.Prefix)
+			currentNode = currentNode.Branches[idx]
+			keyRunes = keyRunes[i:]
+			nodeRunes = []rune(currentNode.Prefix)
 			goto restart
 		}
 
 		// missmatch at the middle of node prefix, split node.
-		if qry[i] != npfx[i] {
+		if keyRunes[i] != nodeRunes[i] {
 
-			var insertNode = &Node[V]{
-				Prefix:   qry[i:],
+			var newNode = &Node[V]{
+				Prefix:   keyRunes[i:],
 				Value:    value,
 				HasValue: true,
 			}
 
 			var splitNode = &Node[V]{
-				Prefix:   npfx[i:],
-				Value:    node.Value,
-				HasValue: node.HasValue,
-				Branches: node.Branches,
+				Prefix:   nodeRunes[i:],
+				Value:    currentNode.Value,
+				HasValue: currentNode.HasValue,
+				Branches: currentNode.Branches,
 			}
 
-			node.Prefix = qry[:i]
-			node.Value = *new(V)
-			node.HasValue = false
-			if splitNode.Prefix[0] > insertNode.Prefix[0] {
-				node.Branches = Branches[V]{insertNode, splitNode}
+			currentNode.Prefix = keyRunes[:i]
+			currentNode.Value = self.zero
+			currentNode.HasValue = false
+			if splitNode.Prefix[0] > newNode.Prefix[0] {
+				currentNode.Branches = Branches[V]{newNode, splitNode}
 			} else {
-				node.Branches = Branches[V]{splitNode, insertNode}
+				currentNode.Branches = Branches[V]{splitNode, newNode}
 			}
 
-			return *new(V), false
+			return self.zero, false
 		}
 
 		i++
@@ -142,16 +147,18 @@ restart:
 
 // Get returns the value at key and true if it exists or a zero value of V and
 // false if not found.
-func (self Trie[V]) Get(key string) (value V, found bool) {
+//
+// Key must not be empty. If it is Get returns a zero value and false.
+func (self *Trie[V]) Get(key string) (value V, found bool) {
 
 	if key == "" {
-		panic("invalid key, must not be empty")
+		return self.zero, false
 	}
 
 	var qry = []rune(key)
 	var idx int
 	if idx, found = self.root.Branches.find(qry[0]); !found {
-		return *new(V), false
+		return self.zero, false
 	}
 	var node = self.root.Branches[idx]
 	var npfx = []rune(node.Prefix)
@@ -163,14 +170,14 @@ restart:
 			if node.HasValue {
 				return node.Value, true
 			}
-			return *new(V), false
+			return self.zero, false
 		}
 
 		if i == len(npfx) {
 			idx, found = node.Branches.find(qry[i])
 
 			if !found {
-				return *new(V), false
+				return self.zero, false
 			}
 
 			node = node.Branches[idx]
@@ -180,7 +187,7 @@ restart:
 		}
 
 		if qry[i] != npfx[i] {
-			return *new(V), false
+			return self.zero, false
 		}
 
 		i++
@@ -188,17 +195,13 @@ restart:
 }
 
 // Exists returns true if key exists.
-func (self Trie[V]) Exists(key string) (exists bool) {
+func (self *Trie[V]) Exists(key string) (exists bool) {
 	_, exists = self.Get(key)
 	return
 }
 
 // Prefixes returns a list of set keys which are a prefix of key.
-func (self Trie[V]) Prefixes(key string) (out []string) {
-
-	if key == "" {
-		panic("invalid key, must not be empty")
-	}
+func (self *Trie[V]) Prefixes(key string) (out []string) {
 
 	var scanned []rune
 
@@ -252,11 +255,7 @@ restart:
 }
 
 // HasPrefixes returns true if key has any prefixes.
-func (self Trie[V]) HasPrefixes(key string) bool {
-
-	if key == "" {
-		panic("invalid key, must not be empty")
-	}
+func (self *Trie[V]) HasPrefixes(key string) bool {
 
 	var qry = []rune(key)
 	var idx, found = self.root.Branches.find(qry[0])
@@ -297,16 +296,16 @@ restart:
 	}
 }
 
-// Print writes self to writer w as a multiline string representing the tree 
+// Print writes self to writer w as a multiline string representing the tree
 // structure.
 //
-// It is formatted as one node per line where child nodes are indented with two 
+// It is formatted as one node per line where child nodes are indented with two
 // spaces each level and line is in format: <indent><prefix>[,value]
-func (self Trie[V]) Print(w io.Writer)  {
+func (self *Trie[V]) Print(w io.Writer) {
 	self.print(w, self.root, 0)
 }
 
-func (self Trie[V]) print(w io.Writer, n *Node[V], indent int) {
+func (self *Trie[V]) print(w io.Writer, n *Node[V], indent int) {
 	fmt.Fprintf(w, "%s%s", mkindent(indent), string(n.Prefix))
 	if n.HasValue {
 		fmt.Fprintf(w, ",%v", n.Value)
@@ -358,4 +357,77 @@ func binSearch(n int, cmp func(int) int) (i int, found bool) {
 		}
 	}
 	return i, i < n && cmp(i) == 0
+}
+
+// SyncTrie is the concurrency safe version of [Trie].
+type SyncTrie[V any] struct {
+	mutex sync.RWMutex
+	trie  Trie[V]
+}
+
+// NewSyncTrie returns a new [SyncTrie].
+func NewSyncTrie[V any]() *SyncTrie[V] {
+	return &SyncTrie[V]{
+		trie: *New[V](),
+	}
+}
+
+// Put inserts value under key.
+//
+// If a value already exists at key it is returned with true, otherwise a zero
+// value of V is returned and false.
+//
+// Key must not be empty. If it is no value is inserted and a zero value of V
+// and false is returned.
+func (self *SyncTrie[V]) Put(key string, value V) (old V, replaced bool) {
+	self.mutex.Lock()
+	old, replaced = self.trie.Put(key, value)
+	self.mutex.Unlock()
+	return
+}
+
+// Get returns the value at key and true if it exists or a zero value of V and
+// false if not found.
+//
+// Key must not be empty. If it is Get returns a zero value and false.
+func (self *SyncTrie[V]) Get(key string) (value V, found bool) {
+	self.mutex.RLock()
+	value, found = self.trie.Get(key)
+	self.mutex.Unlock()
+	return
+}
+
+// Exists returns true if key exists.
+func (self *SyncTrie[V]) Exists(key string) (exists bool) {
+	self.mutex.RLock()
+	exists = self.trie.Exists(key)
+	self.mutex.RUnlock()
+	return
+}
+
+// Prefixes returns a list of set keys which are a prefix of key.
+func (self *SyncTrie[V]) Prefixes(key string) (out []string) {
+	self.mutex.RLock()
+	out = self.trie.Prefixes(key)
+	self.mutex.RUnlock()
+	return
+}
+
+// HasPrefixes returns true if key has any prefixes.
+func (self *SyncTrie[V]) HasPrefixes(key string) (truth bool) {
+	self.mutex.RLock()
+	truth = self.trie.HasPrefixes(key)
+	self.mutex.RUnlock()
+	return
+}
+
+// Print writes self to writer w as a multiline string representing the tree
+// structure.
+//
+// It is formatted as one node per line where child nodes are indented with two
+// spaces each level and line is in format: <indent><prefix>[,value]
+func (self *SyncTrie[V]) Print(w io.Writer) {
+	self.mutex.RLock()
+	self.trie.Print(w)
+	self.mutex.RUnlock()
 }
